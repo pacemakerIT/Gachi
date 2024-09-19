cf831ded5bd050636ffbd8ba8da12d229a1c3eca
Mirim Kim <mirimkim00@gmail.com>
2024-09-19 13:40:37 -0400

    Add basic component code

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+
+export default function About() {
+  return (
+    <Container maxWidth="lg">
+      <div>About page</div>
+    </Container>
+  );
+}
