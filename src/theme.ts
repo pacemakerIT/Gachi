74dca9e054fddb9b6b1dc8ebd01cd919456e3c31
ngnlyoosun <yoosun10057@gmail.com>
2024-09-30 23:54:47 -0400

    Resolved merge conflicts in src/theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,54 +1,11 @@
-"use client"
+'use client';
 import { createTheme } from '@mui/material/styles';
 import '@fontsource/ibm-plex-sans-kr';
-
-// 타입 확장
-declare module '@mui/material/styles' {
-  interface Palette {
-    customColor: {
-      darkWhite: string; 
-      gray: string;
-    };
-    customBackground: {
-      overlay: string;
-    };
-  }
-
-  interface PaletteOptions {
-    customColor?: {
-      darkWhite?: string;
-      gray?: string;
-    };
-    customBackground?: {
-      overlay?: string;
-    };
-  }
-}
 
 const theme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-<<<<<<< HEAD
-      main: '#2986FE',
-    },
-    secondary: {
-      main: '#101010',
-    },
-    background: {
-      default: '#F9F9FB',
-    },
-    text: {
-      primary: '#101010',
-      secondary: '#F9F9FB',
-    },
-    customColor: {
-      darkWhite: '#F0F2F3', 
-      gray: '#999999',  
-    },
-    customBackground: {
-      overlay: 'rgba(0, 0, 0, 0.5)', 
-=======
       main: '#F9F9FB', // Light background for the AppBar
       dark: '#2986FE', // For buttons
     },
@@ -64,7 +21,6 @@ const theme = createTheme({
     },
     action: {
       hover: '#f1f1f1', // Slight hover effect for icon buttons
->>>>>>> origin/Staging
     },
   },
   typography: {
@@ -79,8 +35,6 @@ const theme = createTheme({
       color: '#101010', // Text color for nav links
     },
   },
-<<<<<<< HEAD
-=======
   components: {
     MuiAppBar: {
       styleOverrides: {
@@ -150,7 +104,7 @@ const theme = createTheme({
       },
     },
   },
->>>>>>> origin/Staging
 });
 
 export default theme;
+
