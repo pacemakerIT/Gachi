0e1193249b59fd892d721d875a2bf0a866cb47b9
Mirim Kim <mirimkim00@gmail.com>
2024-10-03 23:40:13 -0400

    Change img to Image in footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react';
-import { Box, Typography, TextField, Button, IconButton, Container, Link,useMediaQuery } from '@mui/material';
+import Image from 'next/image';
+import { Box, Typography, TextField, Button, IconButton, Container, Link, useMediaQuery } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import { Facebook, Twitter, Instagram, Pinterest, YouTube } from '@mui/icons-material';
 import { useTheme } from '@mui/material/styles';
@@ -32,10 +33,19 @@ interface CategoryAccountSectionProps {
 const LogoSnsSection: React.FC<LogoSectionProps> = ({ socialMediaLinks }) => {
     const theme = useTheme();
     return (
-        <Grid size={{md:4 }} sx={{ textAlign: 'left' }}>
+        <Grid size={{ md: 4 }} sx={{ textAlign: 'left' }}>
             <Box>
                 <Box sx={{ display: 'flex', alignItems: 'center', mb: '1rem' }}>
-                    <img src={logoUrl} alt="Gachi.live Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
+                    <Image
+                        src={logoUrl}
+                        alt='Gachi.live Logo'
+                        width={40}
+                        height={40}
+                        style={{
+                            marginRight: '10px'
+                        }}
+                    />
+
                     <Typography variant="h6" gutterBottom>
                         Gachi.live
                     </Typography>
@@ -59,25 +69,33 @@ const LogoSnsSection: React.FC<LogoSectionProps> = ({ socialMediaLinks }) => {
 // Social Media Section
 const SocialMediaSection: React.FC<{ socialMediaLinksTop: SocialMediaLink[], socialMediaLinksBottom: SocialMediaLink[] }> = ({ socialMediaLinksTop, socialMediaLinksBottom }) => {
     return (
-        <Grid container justifyContent="space-around" alignItems="center" 
-            sx={{ 
-                textAlign: 'justify', 
-                mt: '0.1rem', 
-                minWidth: '100%', 
-                }}
+        <Grid container justifyContent="space-around" alignItems="center"
+            sx={{
+                textAlign: 'justify',
+                mt: '0.1rem',
+                minWidth: '100%',
+            }}
         >
-            <Box 
-                sx={{ 
-                    display: 'flex', 
-                    flexDirection: 'column', 
-                    alignItems: 'flex-start', 
-                    mb: 3, 
-                    paddingLeft: '-1rem' 
-                    }}
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    mb: 3,
+                    paddingLeft: '-1rem'
+                }}
             >
                 <Link href='/' underline='none'>
                     <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-                        <img src="/images/logo.png" alt="Gachi.live Logo" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
+                        <Image
+                            src={logoUrl}
+                            alt='Gachi.live Logo'
+                            width={40}
+                            height={40}
+                            style={{
+                                marginRight: '10px'
+                            }}
+                        />
                         <Typography variant="h6" sx={{ fontWeight: 'bold' }}>Gachi.live</Typography>
                     </Box>
                 </Link>
@@ -86,12 +104,12 @@ const SocialMediaSection: React.FC<{ socialMediaLinksTop: SocialMediaLink[], soc
                 </Link>
             </Box>
 
-            <Box sx={{ 
-                    display: 'grid', 
-                    gridTemplateColumns: 'repeat(3, 1fr)', 
-                    gap: '1rem', 
-                    justifyItems: 'center' 
-                    }}
+            <Box sx={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(3, 1fr)',
+                gap: '1rem',
+                justifyItems: 'center'
+            }}
             >
                 <Box sx={{ gridColumn: 'span 2' }}>
                     {socialMediaLinksTop.map((link, index) => (
@@ -117,15 +135,15 @@ const SocialMediaSection: React.FC<{ socialMediaLinksTop: SocialMediaLink[], soc
 const CategoryAccountSection: React.FC<CategoryAccountSectionProps> = ({ categories, accountLinks }) => {
     const theme = useTheme();
     return (
-        <Grid size={{ xs: 12, md:3 }} sx={{ textAlign: { xs: 'center', md: 'left' }, display: { xs: 'none', md: 'block' } }}>
+        <Grid size={{ xs: 12, md: 3 }} sx={{ textAlign: { xs: 'center', md: 'left' }, display: { xs: 'none', md: 'block' } }}>
             <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: '2rem' }}>
                 <Box>
-                    <Typography variant="subtitle1" 
-                        sx={{ 
-                            mb: '1rem', 
+                    <Typography variant="subtitle1"
+                        sx={{
+                            mb: '1rem',
                             color: theme.palette.customColor.gray,
-                            fontSize: { xs: '0.875rem', md: '1rem' }, 
-                            }}
+                            fontSize: { xs: '0.875rem', md: '1rem' },
+                        }}
                     >
                         CATEGORY
                     </Typography>
@@ -138,12 +156,12 @@ const CategoryAccountSection: React.FC<CategoryAccountSectionProps> = ({ categor
                     ))}
                 </Box>
                 <Box>
-                    <Typography variant="subtitle1" 
-                        sx={{ 
-                            mb: '1rem', 
+                    <Typography variant="subtitle1"
+                        sx={{
+                            mb: '1rem',
                             color: theme.palette.customColor.gray,
-                            fontSize: { xs: '0.875rem', md: '1rem' } 
-                            }}
+                            fontSize: { xs: '0.875rem', md: '1rem' }
+                        }}
                     >
                         YOUR ACCOUNT
                     </Typography>
@@ -166,35 +184,35 @@ const NewsletterSection = () => {
     const isMobile = useMediaQuery(theme.breakpoints.down('md')); //Mobile
 
     return (
-        <Grid size={{ xxs: 12, md:4 }}
+        <Grid size={{ xxs: 12, md: 4 }}
             sx={{
                 textAlign: isMobile ? 'center' : 'left',
                 margin: isMobile ? '1rem' : '0',
                 ml: !isMobile ? '4rem' : '0',
             }}
         >
-                {isMobile ? (
-                        <Typography
-                            variant='h6'
-                            sx={{ 
-                                color: theme.palette.text.secondary,
-                                mb: '1rem',
-                                mt: '-1rem'
-                             }} 
-                        >
-                            뉴스레터를 통해 <br /> 매주 새로운 소식을 만나보세요!
-                        </Typography>
-                ) : (
-                    <Typography
-                        variant='subtitle1'
-                        sx={{ color: theme.palette.customColor?.gray, mb: '1rem' }} 
-                    >
-                        NEWSLETTER
-                    </Typography>
-                )}
+            {isMobile ? (
+                <Typography
+                    variant='h6'
+                    sx={{
+                        color: theme.palette.text.secondary,
+                        mb: '1rem',
+                        mt: '-1rem'
+                    }}
+                >
+                    뉴스레터를 통해 <br /> 매주 새로운 소식을 만나보세요!
+                </Typography>
+            ) : (
+                <Typography
+                    variant='subtitle1'
+                    sx={{ color: theme.palette.customColor?.gray, mb: '1rem' }}
+                >
+                    NEWSLETTER
+                </Typography>
+            )}
 
-            {isMobile &&(
-                    <Typography
+            {isMobile && (
+                <Typography
                     variant="body2"
                     sx={{
                         color: theme.palette.customColor?.gray,
@@ -223,11 +241,11 @@ const NewsletterSection = () => {
                     variant="outlined"
                     label="Your email"
                     size="small"
-                    fullWidth={isMobile} 
+                    fullWidth={isMobile}
                     sx={{
                         flex: 1,
                         maxWidth: isMobile ? '90%' : '300px',
-                        mb:{xxs:'0.5rem', md:0},
+                        mb: { xxs: '0.5rem', md: 0 },
                     }}
                 />
                 <Button
@@ -236,9 +254,9 @@ const NewsletterSection = () => {
                         backgroundColor: theme.palette.primary.main,
                         whiteSpace: 'nowrap',
                         width: isMobile ? '90%' : 'auto',
-                        flexShrink: {md:0},
+                        flexShrink: { md: 0 },
                         borderRadius: '8px',
-                       
+
                     }}
                 >
                     Subscribe
@@ -262,7 +280,7 @@ const NewsletterSection = () => {
 // Footer Main Component
 export default function Footer() {
     const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('md')); 
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
     const socialMediaLinks: SocialMediaLink[] = [
         { href: 'https://www.facebook.com', icon: <Facebook /> },
@@ -273,7 +291,7 @@ export default function Footer() {
     ];
 
     const socialMediaLinksTop = socialMediaLinks.slice(0, 2);
-    const socialMediaLinksBottom = socialMediaLinks.slice(2); 
+    const socialMediaLinksBottom = socialMediaLinks.slice(2);
 
     const categories = [
         { name: 'Home', href: '/' },
@@ -291,10 +309,10 @@ export default function Footer() {
 
 
     return (
-        <Box component="footer" sx={{ padding: '2rem 0' , width:'100%' }}>
-            {isMobile ?(
+        <Box component="footer" sx={{ padding: '2rem 0', width: '100%' }}>
+            {isMobile ? (
                 //Mobile view
-                <Container maxWidth="sm" sx={{paddingX: '1.5rem'}}>
+                <Container maxWidth="sm" sx={{ paddingX: '1.5rem' }}>
                     <Grid container spacing={4} justifyContent="center" alignItems="center">
                         {/* Newsletter section */}
                         <NewsletterSection />
@@ -302,21 +320,21 @@ export default function Footer() {
                         {/* Social Media Section */}
                         <SocialMediaSection socialMediaLinksTop={socialMediaLinksTop} socialMediaLinksBottom={socialMediaLinksBottom} />
                     </Grid>
-            </Container>
+                </Container>
 
-            ): (
+            ) : (
 
                 //Desktop view
-                <Container maxWidth="lg" sx={{paddingX: '1rem'}}>
-                    <Grid container spacing={4} alignItems="flex-start" justifyContent="center" sx={{mt:'1.5rem',mb:'2rem'}}>
+                <Container maxWidth="lg" sx={{ paddingX: '1rem' }}>
+                    <Grid container spacing={4} alignItems="flex-start" justifyContent="center" sx={{ mt: '1.5rem', mb: '2rem' }}>
                         <LogoSnsSection socialMediaLinks={socialMediaLinks} />
                         <CategoryAccountSection categories={categories} accountLinks={accountLinks} />
                         <NewsletterSection />
                     </Grid>
                 </Container>
             )
-            } 
-  
+            }
+
         </Box>
     );
 }
