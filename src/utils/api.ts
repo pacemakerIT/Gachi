9930a95c97ef65734f3a9a873a6b304389948bc6
Mirim Kim <mirimkim00@gmail.com>
2024-11-08 16:36:42 -0500

    Handle error when failed to fetch

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,13 @@
 import { FetchDataResponse } from './types';
 
-export async function fetchData(): Promise<FetchDataResponse> {
+export async function fetchData(): Promise<FetchDataResponse | null> {
   const res = await fetch(
     'http://127.0.0.1:8000/landing_page/landing-page-supabase/',
     { cache: 'no-store' }
   );
 
   if (!res.ok) {
-    throw new Error('Failed to fetch posts');
+    return null;
   }
   const data = await res.json();
   return data as FetchDataResponse;
