a6e58ba8b0d9717caaf87f7b696740b195ea283a
Sehwan <sehwan.lee.dev@gmail.com>
2024-11-22 11:32:10 -0400

    Removed no-cache option

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,9 +3,7 @@ import { FetchDataResponse } from './types';
 export async function fetchData(): Promise<FetchDataResponse | null> {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-  const res = await fetch(`${baseUrl}/landing_page/supabase/`, {
-    cache: 'no-store',
-  });
+  const res = await fetch(`${baseUrl}/landing_page/supabase/`);
 
   const data = await res.json();
   return data as FetchDataResponse;
