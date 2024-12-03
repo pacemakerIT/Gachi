bc650061dec2472eccdaa69189f7638874191cf9
Mirim Kim <mirimkim00@gmail.com>
2024-12-02 20:05:06 -0500

    Fix error in api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,10 @@
-import { LandingPageDataResponse, DashboardDataResponse } from './types';
+import { LandingPageDataResponse } from './types';
 
-export async function fetchLandingPageData(): Promise<FetchDataResponse | null> {
+export async function fetchLandingPageData(): Promise<LandingPageDataResponse | null> {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
   const res = await fetch(`${baseUrl}/landing_page/supabase/`);
 
   const data = await res.json();
-  return data as FetchDataResponse;
-
+  return data as LandingPageDataResponse;
 }
