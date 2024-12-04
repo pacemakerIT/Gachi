ab78b606d664f2fe0c4e4fb5c85f35d154040710
Sehwan <sehwan.lee.dev@gmail.com>
2024-12-03 22:36:19 -0400

    Differenciate dotenv between local and production

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,28 @@
-import { LandingPageDataResponse, DashboardDataResponse } from './types';
+import { DashboardDataResponse, LandingPageDataResponse } from './types';
 
-export async function fetchData(): Promise<FetchDataResponse | null> {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
+export async function fetchLandingPageData(): Promise<LandingPageDataResponse | null> {
   const res = await fetch(`${baseUrl}/landing_page/supabase/`);
 
   const data = await res.json();
-  return data as FetchDataResponse;
+  return data as LandingPageDataResponse;
+}
 
+// Dashboard 데이터 가져오기
+export async function fetchDashboardData(): Promise<DashboardDataResponse | null> {
+  try {
+    const res = await fetch(`${baseUrl}/dashboard/dashboard_api_design/`);
+
+    if (!res.ok) {
+      console.error('Failed to fetch dashboard data');
+      return null;
+    }
+
+    const data = await res.json();
+    return data as DashboardDataResponse;
+  } catch (error) {
+    console.error('Error fetching dashboard data:', error);
+    return null;
+  }
 }
